2064ce8880a55f8ee7199ec6785345d81f560ed7
Nicolas Perriault <nperriault@gmail.com>
2011-10-09 09:01:14 +0200

    fixed inaccurate example

--- samples/download.js
+++ samples/download.js
@@ -6,7 +6,7 @@ new phantom.Casper({
     verbose: true
 }).start('http://www.google.fr/', function(self) {
     // download the google logo image as base64
-    logo = self.base64encode('http://www.google.fr/images/srpr/logo3w.png', 'google_logo.png');
+    logo = self.base64encode('http://www.google.fr/images/srpr/logo3w.png');
 }).run(function(self) {
     self.echo(logo).exit();
 });
