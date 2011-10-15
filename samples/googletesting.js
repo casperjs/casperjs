59984894240c6cd6bc20585d98819883e95ef318
Nicolas Perriault <nperriault@gmail.com>
2011-10-15 16:44:55 +0200

    fixed Casper#exists(), added Casper.Tester#assertExists() method

---
+++ samples/googletesting.js
@@ -0,0 +1,25 @@
+phantom.injectJs("casper.js");
+
+var casper = new phantom.Casper({
+    logLevel: "debug"
+});
+
+casper.start('http://www.google.fr/', function(self) {
+    self.test.assertTitle('Google', 'google homepage title is the one expected');
+    self.test.assertExists('form[name=f]', 'main form is found');
+    self.fill('form[name=f]', {
+        q: 'foo'
+    }, true);
+});
+
+casper.then(function(self) {
+    self.test.assertTitle('foo - Recherche Google', 'google title is ok');
+    self.test.assertUrlMatch(/q=foo/, 'search term has been submitted');
+    self.test.assertEval(function() {
+        return __utils__.findAll('h3.r').length >= 10;
+    }, 'google search for "foo" retrieves 10 or more results');
+});
+
+casper.run(function(self) {
+    self.test.renderResults(true);
+});
