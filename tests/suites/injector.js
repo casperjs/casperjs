dd321f7e76897bb7d00d75ea24776d11deac2e11
Nicolas Perriault <nperriault@gmail.com>
2011-12-12 23:41:41 +0100

    new tester can handle coffeescript test files as well

--- tests/suites/injector.js
+++ tests/suites/injector.js
@@ -1,23 +1,22 @@
-// phantom.Casper.FunctionArgsInjector
-var t = casper.test;
+(function(t) {
+    function createInjector(fn, values) {
+        return new phantom.Casper.FunctionArgsInjector(fn, values);
+    }
 
-function createInjector(fn, values) {
-    return new phantom.Casper.FunctionArgsInjector(fn, values);
-}
+    var testFn = function(a, b) { return a + b; };
+    var injector = createInjector(testFn);
+    var extract = injector.extract(testFn);
 
-var testFn = function(a, b) { return a + b; };
-var injector = createInjector(testFn);
-var extract = injector.extract(testFn);
+    t.comment('FunctionArgsInjector.extract()');
+    t.assertType(extract, "object", 'FunctionArgsInjector.extract() returns an object');
+    t.assertEquals(extract.name, null, 'FunctionArgsInjector.extract() process function name as expected');
+    t.assertEquals(extract.body, 'return a + b;', 'FunctionArgsInjector.extract() process function body as expected');
+    t.assertEquals(extract.args, ['a', 'b'], 'FunctionArgsInjector.extract() process function args as expected');
 
-t.comment('FunctionArgsInjector.extract()');
-t.assertType(extract, "object", 'FunctionArgsInjector.extract() returns an object');
-t.assertEquals(extract.name, null, 'FunctionArgsInjector.extract() process function name as expected');
-t.assertEquals(extract.body, 'return a + b;', 'FunctionArgsInjector.extract() process function body as expected');
-t.assertEquals(extract.args, ['a', 'b'], 'FunctionArgsInjector.extract() process function args as expected');
+    var processed;
+    eval('processed = ' + injector.process({ a: 1, b: 2 }));
 
-var processed;
-eval('processed = ' + injector.process({ a: 1, b: 2 }));
+    t.assertEquals(processed(), 3, 'FunctionArgsInjector.process() proccessed the function correctly');
 
-t.assertEquals(processed(), 3, 'FunctionArgsInjector.process() proccessed the function correctly');
-
-t.done();
+    t.done();
+})(casper.test);
