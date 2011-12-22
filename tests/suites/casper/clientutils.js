1932b913623cec169179cb37c8dbabf2d0871ad8
Nicolas Perriault <nperriault@gmail.com>
2011-12-22 12:18:28 +0100

    attempt to add a dowload feature to Casper

---
+++ tests/suites/casper/clientutils.js
@@ -0,0 +1,24 @@
+(function(t) {
+    t.comment('ClientUtils.encode()');
+
+    var fs = require('fs');
+    var utils = new phantom.Casper.ClientUtils();
+    var testCases = {
+        'an empty string': '',
+        'a word':          'plop',
+        'an utf8 string':  '<chè!cs!<egfhèqzgrqgzf',
+        'song lyrics':     ("Voilà l'été, j'aperçois le soleil\n" +
+                            "Les nuages filent et le ciel s'éclaircit\n" +
+                            "Et dans ma tête qui bourdonnent?\n" +
+                            "Les abeilles!"),
+        'a file contents': fs.read(phantom.casperPath + '/tests/site/images/phantom.png')
+    };
+
+    for (var what in testCases) {
+        var source = testCases[what];
+        var encoded = utils.encode(source);
+        t.assertEquals(utils.decode(encoded), source, 'ClientUtils can encode and decode ' + what);
+    }
+
+    t.done();
+})(casper.test);
--- tests/suites/casper/encode.js
+++ tests/suites/casper/encode.js
@@ -2,8 +2,12 @@
     t.comment('Casper.base64encode()');
 
     casper.start('tests/site/index.html', function(self) {
-        var image = self.base64encode('file://' + phantom.casperPath + '/tests/site/images/phantom.png');
+        var imageUrl = 'file://' + phantom.casperPath + '/tests/site/images/phantom.png';
+        var image = self.base64encode(imageUrl);
         t.assertEquals(image.length, 6160, 'Casper.base64encode() can retrieve base64 contents');
+        self.download(imageUrl, 'logo.png');
+        var fs = require('fs');
+        t.assert(fs.exists('logo.png'), 'Casper.download() downloads a file');
     });
 
     casper.run(function(self) {
