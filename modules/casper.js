casperjs/casperjs

commit dd5c9fdaeb0e3d86270a8e85afb6db833d904fa7

hunting unnecessary selves (!)

modules/casper.js
@@ -376,11 +376,9 @@ Casper.prototype.each = function each(array, fn) {
         this.log("each() only works with arrays", "error");
         return this;
     }
-    (function _each(self) {
-        array.forEach(function _forEach(item, i) {
-            fn.call(self, self, item, i);
-        });
-    })(this);
+    array.forEach.call(this, function _forEach(item, i) {
+        fn.call(this, this, item, i);
+    });
     return this;
 };
 
@@ -922,8 +920,8 @@ Casper.prototype.start = function start(location, then) {
         }, this.options.timeout, this);
     }
     if (utils.isString(location) && location.length > 0) {
-        return this.thenOpen(location, utils.isFunction(then) ? then : this.createStep(function _step(self) {
-            self.log("start page is loaded", "debug");
+        return this.thenOpen(location, utils.isFunction(then) ? then : this.createStep(function _step() {
+            this.log("start page is loaded", "debug");
         }));
     }
     return this;

commit e5d13edf5010c98cd59eab26b04c0c6b90d4878a

fixed `each()` method which was broken
regression occured in dd5c9fdaeb0e3d86270a8e85afb6db833d904fa7

modules/casper.js
@@ -226,11 +226,7 @@ Casper.prototype.checkStep = function checkStep(self, onComplete) {
         clearInterval(self.checker);
         self.emit('run.complete');
         if (utils.isFunction(onComplete)) {
-            try {
-                onComplete.call(self, self);
-            } catch (err) {
-                self.log("Could not complete final step: " + err, "error");
-            }
+            onComplete.call(self, self);
         } else {
             // default behavior is to exit
             self.exit();
@@ -375,9 +371,11 @@ Casper.prototype.each = function each(array, fn) {
         this.log("each() only works with arrays", "error");
         return this;
     }
-    array.forEach.call(this, function _forEach(item, i) {
-        fn.call(this, this, item, i);
-    });
+    (function _each(self) {
+        array.forEach(function _forEach(item, i) {
+            fn.call(self, self, item, i);
+        });
+    })(this);
     return this;
 };
 