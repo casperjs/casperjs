608108eeef2fd29ebf739c61abf4a7a12181531c
Nicolas Perriault <nperriault@gmail.com>
2013-02-25 01:23:08 +0100

    request tests uses setUp() and tearDown()

--- tests/suites/casper/request.js
+++ tests/suites/casper/request.js
@@ -1,42 +1,48 @@
 /*global casper*/
 /*jshint strict:false*/
+var currentRequest;
+
+function onResourceRequested(request) {
+    currentRequest = request;
+}
+
 function testHeader(header) {
     return header.name === 'Accept' && header.value === 'application/json';
 }
 
-casper.test.begin('requests tests', 3, function(test) {
-    var current = 0,
-        tests = [
-            function(request) {
-                test.assertNot(request.headers.some(testHeader),
-                    "Casper.open() sets no custom header by default");
-            },
-            function(request) {
-                test.assert(request.headers.some(testHeader),
-                    "Casper.open() can set a custom header");
-            },
-            function(request) {
-                test.assertNot(request.headers.some(testHeader),
-                    "Casper.open() custom headers option is not persistent");
+casper.test.begin('requests tests', 3, {
+    setUp: function() {
+        casper.on('page.resource.requested', onResourceRequested);
+    },
+
+    tearDown: function() {
+        currentRequest = undefined;
+        casper.removeListener('page.resource.requested', onResourceRequested);
+    },
+
+    test: function(test) {
+        casper.start('tests/site/index.html', function() {
+            test.assertNot(currentRequest.headers.some(testHeader),
+                "Casper.open() sets no custom header by default");
+        });
+
+        casper.thenOpen('tests/site/index.html', {
+            headers: {
+                Accept: 'application/json'
             }
-        ];
+        }, function() {
+            test.assert(currentRequest.headers.some(testHeader),
+                "Casper.open() can set a custom header");
+        });
 
-    casper.on('page.resource.requested', function(request) {
-        tests[current++](request);
-    });
+        casper.thenOpen('tests/site/index.html', function() {
+            test.assertNot(currentRequest.headers.some(testHeader),
+                "Casper.open() custom headers option is not persistent");
+        });
 
-    casper.start('tests/site/index.html');
-
-    casper.thenOpen('tests/site/index.html', {
-        headers: {
-            Accept: 'application/json'
-        }
-    });
-
-    casper.thenOpen('tests/site/index.html');
-
-    casper.run(function() {
-        this.removeAllListeners('page.resource.requested');
-        test.done();
-    });
+        casper.run(function() {
+            this.removeAllListeners('page.resource.requested');
+            test.done();
+        });
+    }
 });
