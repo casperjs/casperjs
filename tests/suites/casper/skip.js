66031b0305e5435d8e960037e2aba4ad37f416b4
Julien Muetton <themouette@gmail.com>
2013-03-07 00:54:07 +0100

    Add `casper.thenSkip()`, `casper.thenSkipIf()`...
    ... and `casper.thenSkipUnless()`

--- tests/suites/casper/skip.js
+++ tests/suites/casper/skip.js
@@ -61,3 +61,78 @@ casper.test.begin('Skip does not polluate next suite', 1, function(test) {
     });
 });
 
+casper.test.begin('Casper.thenSkip', 2, function(test) {
+    casper.
+        thenSkip(1).
+        then(function () {
+            test.fail("This test should be skipped.");
+        }).
+        then(function () {
+            test.pass("This test should be executed.");
+        });
+
+    casper.run(function() {
+        test.done();
+    });
+});
+
+casper.test.begin('Casper.thenSkipIf', 5, function(test) {
+    casper.
+        thenSkipIf(true, 1, "Skip if with function").
+        then(function () {
+            test.fail("This test should be skipped.");
+        }).
+        then(function () {
+            test.pass("This test should be executed.");
+        }).
+        thenSkipIf(function () {
+            return true;
+            }, 1, "Skip if with function").
+        then(function () {
+            test.fail("This test should be skipped.");
+        }).
+        then(function () {
+            test.pass("This test should be executed.");
+        }).
+        thenSkipIf(function () {
+            return false;
+            }, 1, "Do not skip if with function").
+        then(function () {
+            test.pass("This test should be executed.");
+        });
+
+    casper.run(function() {
+        test.done();
+    });
+});
+
+casper.test.begin('Casper.thenSkipUnless', 5, function(test) {
+    casper.
+        thenSkipUnless(false, 1, "Skip unless with function").
+        then(function () {
+            test.fail("This test should be skipped.");
+        }).
+        then(function () {
+            test.pass("This test should be executed.");
+        }).
+        thenSkipUnless(function () {
+            return false;
+            }, 1, "Skip unless with function").
+        then(function () {
+            test.fail("This test should be skipped.");
+        }).
+        then(function () {
+            test.pass("This test should be executed.");
+        }).
+        thenSkipUnless(function () {
+            return true;
+            }, 1, "Do not skip unless with function").
+        then(function () {
+            test.pass("This test should be executed.");
+        });
+
+    casper.run(function() {
+        test.done();
+    });
+});
+
