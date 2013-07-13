df2466375418e08dfcc51bde00ae790852b9905a
Nicolas Perriault <nperriault@gmail.com>
2013-07-13 16:22:40 +0200

    Revert "added checks for PhantomJS version; updated travis build config"
    This reverts commit 4c4fb34b149e8e72906c9bb44dc023699b2301ab.
    
    Erroneous branch, pushing releases while drinking wine is not probably not a good idea.

--- bin/bootstrap.js
+++ bin/bootstrap.js
@@ -37,12 +37,9 @@ if ('process' in this && process.title === "node") {
     process.exit(1);
 }
 
-// phantom checks
+// phantom check
 if (!('phantom' in this)) {
     console.error('CasperJS needs to be executed in a PhantomJS environment http://phantomjs.org/');
-} else if (phantom.version.major >= 1 && phantom.version.minor >= 9) {
-    console.error('CasperJS 1.0.x does not support PhantomJS version >= 1.9');
-    phantom.exit();
 }
 
 // Common polyfills
