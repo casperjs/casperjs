fd18db00af7f1ec560e45501d05e4247649be9bd
QWp6t <QWp6t+github@xpdnc.org>
2013-08-04 12:30:32 -0700

    refs #562 - Fix backslashes acting as escape chars

--- batchbin/casperjs.bat
+++ batchbin/casperjs.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 set CASPER_PATH=%~dp0..
-set CASPER_BIN=%CASPER_PATH%\bin\
-set ARGV=%*
-call phantomjs "%CASPER_BIN%bootstrap.js" --casper-path="%CASPER_PATH%" --cli %ARGV%
+set CASPER_PATH=%CASPER_PATH:\=/%
+set CASPER_BIN=%CASPER_PATH%/bin/
+call phantomjs "%CASPER_BIN%bootstrap.js" --casper-path="%CASPER_PATH%" --cli %*
