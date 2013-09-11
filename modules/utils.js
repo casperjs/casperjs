4b60a5dacb67b79f1cb07e88f57cd8e6733a2bd3
Mickaël Andrieu <andrieu.travail@gmail.com>
2013-09-11 10:51:06 +0200

    Finaly, fixed jshint issue by @n1k0
    Thanks

--- modules/utils.js
+++ modules/utils.js
@@ -80,7 +80,7 @@ exports.betterTypeOf = betterTypeOf;
  */
 function betterInstanceOf(input, constructor) {
     "use strict";
-    /*jshint eqnull:true, eqeqeq:false, undef:false */
+    /*jshint eqnull:true, eqeqeq:false, global XMLDocument */
     while (input != null) {
       if (input == constructor.prototype) {
         return true;
