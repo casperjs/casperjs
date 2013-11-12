f3d82877605906b138c54e52329cb593319f5464
Clochix <clochix@clochix.net>
2013-11-12 18:23:23 +0100

    Update deprecated frame methods

--- tests/suites/casper/frames.js
+++ tests/suites/casper/frames.js
@@ -1,6 +1,6 @@
 /*global casper, __utils__*/
 /*jshint strict:false*/
-casper.test.begin('handling frames', 16, function(test) {
+casper.test.begin('handling frames', 20, function(test) {
     casper.start('tests/site/frames.html');
 
     casper.withFrame('frame1', function() {
@@ -36,6 +36,22 @@ casper.test.begin('handling frames', 16, function(test) {
 
     casper.withFrame(1, function() {
         test.assertTitle('CasperJS frame 3');
+    });
+
+
+    casper.then(function() {
+      casper.page.switchToMainFrame();
+      casper.on('frame.changed', function (name) {
+        test.assertEquals(name, 'frame1');
+      });
+      casper.switchToFrame("frame1");
+      test.assertTitle('CasperJS frame 1');
+      casper.then(function() {
+        // Same frame in next step
+        test.assertTitle('CasperJS frame 1');
+        casper.page.switchToMainFrame();
+        test.assertTitle("CasperJS test frames");
+      });
     });
 
     casper.run(function() {
