38d276119c4125b174d78048907c7a1aaa0cfb7e
Nicolas Perriault <nperriault@gmail.com>
2013-12-10 10:42:16 +0100

    updated xunit tests to refect use of getSerializedXML

--- modules/xunit.js
+++ modules/xunit.js
@@ -159,6 +159,7 @@ XUnitExporter.prototype.getXML = function getXML() {
  * @return string
  */
 XUnitExporter.prototype.getSerializedXML = function getSerializedXML(xml) {
+    "use strict";
     var serializer = new XMLSerializer();
     return '<?xml version="1.0" encoding="UTF-8"?>' + serializer.serializeToString(this.getXML());
 }
