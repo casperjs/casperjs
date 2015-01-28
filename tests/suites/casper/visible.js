156745544a814857129fe5599fe3801f28df6eca
Jukka Paasonen <paazmaya@yahoo.com>
2015-01-28 14:22:08 +0200

    Handle flex and inline-flex for reading visible state

--- tests/suites/casper/visible.js
+++ tests/suites/casper/visible.js
@@ -1,5 +1,5 @@
 /*eslint strict:0*/
-casper.test.begin('visibility tests', 5, function(test) {
+casper.test.begin('visibility tests', 6, function(test) {
     casper.start('tests/site/visible.html', function() {
         test.assert(!this.visible('#img1'), 'Casper.visible() can detect if an element is invisible');
         test.assert(this.visible('#img2'), 'Casper.visible() can detect if an element is visible');
