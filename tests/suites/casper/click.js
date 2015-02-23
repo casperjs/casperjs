35f3851c405fc4b5d6c01488269100d351b96c96
badriub@gmail.com <badriub@gmail.com>
2015-02-23 17:10:16 -0500

    refs #1162 - added right click mouse click option
    This patch enables casperjs to bring up the context menu on right click.

--- tests/suites/casper/click.js
+++ tests/suites/casper/click.js
@@ -115,7 +115,7 @@ casper.test.begin('mouse events on click', 3, function(test) {
     casper.start('tests/site/click.html', function() {
         this.click('#test5');
     }).then(function() {
-        var results = this.getGlobal('results');
+        var results = this.getGlobal('results');        
         test.assert(results.test5.indexOf('mousedown') !== -1,
             'Casper.click() triggers mousedown event');
         test.assert(results.test5.indexOf('mouseup') !== -1,
@@ -126,3 +126,30 @@ casper.test.begin('mouse events on click', 3, function(test) {
         test.done();
     });
 });
+
+casper.test.begin('mouse events on right click', 1, function(test) {
+    casper.start('tests/site/click.html', function() {
+        this.mouse.rightclick('#test5');
+    }).then(function() {
+        var results = this.getGlobal('results');        
+
+        test.assert(results.test5.indexOf('contextmenu') !== -1,
+            'Casper.rightclick() triggers contextmenu event');    
+    }).run(function() {
+        test.done();
+    });
+});
+
+casper.test.begin('mouse events on right click with x,y co-ordinates', 1, function(test) {
+    casper.start('tests/site/click.html', function() {
+        this.mouse.rightclick(200, 100);
+    }).then(function() {
+        var results = this.getGlobal('results');        
+
+        test.assertEquals(results.testrightclick, [200, 100],
+            'Mouse.move() has moved to the specified position');              
+    }).run(function() {
+        test.done();
+    });
+});
+
