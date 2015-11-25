f5388801bab5ac6641aa732b8fc816289a1e16a9
Ingo Struck <git@ingostruck.de>
2015-11-25 17:04:37 +0100

    remove spurious javascriptEnabled setting

--- tests/suites/casper/prompt.js
+++ tests/suites/casper/prompt.js
@@ -3,7 +3,6 @@ casper.test.begin('prompt tests', 1, function(test) {
     casper.setFilter('page.prompt', function(message, value) {
         return 'Chuck ' + value;
     });
-    casper.options.pageSettings.javascriptEnabled = true;
     casper.start('tests/site/prompt.html', function() {
         test.assertEquals(this.getGlobal('username'), 'Chuck Norris', 'prompted value has been received');
     }).run(function() {
