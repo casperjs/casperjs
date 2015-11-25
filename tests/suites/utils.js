2b82eaaa33e0f478cd290970237b59baa6a66c14
Ingo Struck <git@ingostruck.de>
2015-11-25 16:56:56 +0100

    More concise .coffee support check https://github.com/n1k0/casperjs/pull/1352#discussion-diff-45838614

--- tests/suites/utils.js
+++ tests/suites/utils.js
@@ -314,7 +314,7 @@ casper.test.begin('isJsFile() tests', 5, function(test) {
         '':             false,
         'toto.png':     false,
         'plop':         false,
-        'gniii.coffee': Object.keys(require.extensions).indexOf('.coffee') !== -1,
+        'gniii.coffee': '.coffee' in require.extensions,
         'script.js':    true
     };
     for (var testCase in testCases) {
