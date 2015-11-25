e194b170af95bb9fa2ea373821c1cd51bcafff60
Ingo Struck <git@ingostruck.de>
2015-11-25 16:34:13 +0100

    use utils.matchEngine

--- tests/suites/tester/assert.js
+++ tests/suites/tester/assert.js
@@ -1,19 +1,6 @@
 /*eslint strict:0, max-statements:0*/
 var fs = require('fs');
 
-/**
- * Known regression in 2.0.0, will be fixed in 2.0.1
- * https://github.com/ariya/phantomjs/issues/12506
- */
-function isPhantom200() {
-    if (phantom.casperEngine !== 'phantomjs') {
-        return false;
-    }
-    var version = phantom.version;
-    return 2 === version.major &&
-           0 === version.minor && 0 === version.patch;
-}
-
 casper.test.begin('Common assertions tests', 47, function(test) {
     casper.start('tests/site/index.html', function() {
         test.assertTextExists('form', 'Tester.assertTextExists() checks that page body contains text');
@@ -110,9 +97,11 @@ casper.test.begin('Tester.assertField(): filled inputs', 7, function(test) {
 casper.test.begin('Tester.assertField(): unfilled inputs', 7, function(test) {
     var fpath = fs.pathJoin(phantom.casperPath, 'README.md');
     var fileValue = 'README.md';
-    if (phantom.casperEngine === 'phantomjs'
-    ) {
-        if (isPhantom200()) {
+    if (phantom.casperEngine === 'phantomjs') {
+        if (utils.matchEngine({
+            name: 'phantomjs',
+            version: {min: '2.0.0', max: '2.0.0'}
+        })) {
             fileValue = '';
         } else {
             fileValue = 'C:\\fakepath\\README.md'; // phantomjs/webkit sets that;
