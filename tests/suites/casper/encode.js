f2d9794c6b2451183f6668f66a713baab098b0d3
EThaiZone <ethaizone@hotmail.com>
2016-02-02 17:03:48 +0700

    add test case

--- tests/suites/casper/encode.js
+++ tests/suites/casper/encode.js
@@ -16,3 +16,19 @@ casper.test.begin('base64encode() and download() tests', 2, function(test) {
         test.done();
     });
 });
+
+casper.test.begin('binary() and downloadBinary() tests', 2, function(test) {
+    // FIXME: https://github.com/ariya/phantomjs/pull/364 has been merged, update scheme
+    casper.start('file://' + phantom.casperPath + '/tests/site/index.html', function() {
+        var imageUrl = 'file://' + phantom.casperPath + '/tests/site/images/phantom.png',
+            image = this.binary(imageUrl);
+        test.assertEquals(image.length, 6160, 'Casper.binary() can retrieve binary contents');
+        this.downloadBinary(imageUrl, '__test_logo.png');
+        test.assert(fs.exists('__test_logo.png'), 'Casper.downloadBinary() downloads a file');
+        if (fs.exists('__test_logo.png')) {
+            fs.remove('__test_logo.png');
+        }
+    }).run(function() {
+        test.done();
+    });
+});
