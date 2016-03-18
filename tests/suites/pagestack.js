47eb9dbc551d5ebcd50ea0fbe3edcd73f0c52258
Ryan Null <ryan.null@gmail.com>
2016-03-18 13:45:18 -0700

    refs #1354 - fixed by improving clean method
    It was changed so that the clean method is more apt to its name.  It will clean any popups that are not longer valid.

--- tests/suites/pagestack.js
+++ tests/suites/pagestack.js
@@ -23,7 +23,7 @@ casper.test.begin('pagestack module tests', 14, function(test) {
     test.assertEquals(stack.list().length, 2);
     test.assertEquals(stack.list()[1], page2.url);
 
-    test.assertEquals(stack.clean(page1), 1);
+    test.assertEquals(stack.clean(), 1);
     test.assertEquals(stack[0], page2);
     test.assertEquals(stack.list().length, 1);
     test.assertEquals(stack.list()[0], page2.url);
