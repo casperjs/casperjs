f06299ee3020f9184d3119d95e9369a956588648
Mickaël Andrieu <andrieu.travail@gmail.com>
2016-04-11 17:20:15 +0000

    Added tests of encode() functions

--- tests/suites/casper/encode.js
+++ tests/suites/casper/encode.js
@@ -1,7 +1,7 @@
 /*eslint strict:0*/
 var fs = require('fs');
 
-casper.test.begin('base64encode() and download() tests', 2, function(test) {
+casper.test.begin('base64encode() and download() tests', 8, function(test) {
     // FIXME: https://github.com/ariya/phantomjs/pull/364 has been merged, update scheme
     casper.start('file://' + phantom.casperPath + '/tests/site/index.html', function() {
         var imageUrl = 'file://' + phantom.casperPath + '/tests/site/images/phantom.png',
@@ -11,6 +11,29 @@ casper.test.begin('base64encode() and download() tests', 2, function(test) {
         test.assert(fs.exists('__test_logo.png'), 'Casper.download() downloads a file');
         if (fs.exists('__test_logo.png')) {
             fs.remove('__test_logo.png');
+        }
+    })
+    .then(function() {
+        var csvFile = 'file://' + phantom.casperPath + '/tests/site/csv/base64encode.csv';
+        this.download(csvFile, '__base64encode.csv');
+        test.assert(fs.exists('__base64encode.csv'), 'Casper.download() downloads a file');
+
+        var stream = fs.open('__base64encode.csv', 'r'),
+        expectedValues = [
+            'إختبار,',
+            'آزمایشی,',
+            '测试',
+            'испытание',
+            'परीकi'
+        ],
+        i = 0,
+        lines = stream.read().split(/[\n]/);
+        expectedValues.forEach(function(value) {
+            test.assertEquals(lines[i], value, 'Casper.base64encode() can retrieve base64 complex strings');
+            i++;
+        });
+        if (fs.exists('__base64encode.csv')) {
+            fs.remove('__base64encode.csv');
         }
     }).run(function() {
         test.done();
