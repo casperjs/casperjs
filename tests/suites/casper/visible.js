2b8cf259dad496c1b0d1b7ef27d34e9d54714b90
Juga Paazmaya <paazmaya@yahoo.com>
2016-08-13 01:25:44 +0300

    Redone what got messed in #1136

--- tests/suites/casper/visible.js
+++ tests/suites/casper/visible.js
@@ -1,9 +1,10 @@
 /*eslint strict:0*/
-casper.test.begin('visibility tests', 5, function(test) {
+casper.test.begin('visibility tests', 6, function(test) {
     casper.start('tests/site/visible.html', function() {
         test.assert(!this.visible('#img1'), 'Casper.visible() can detect if an element is invisible');
         test.assert(this.visible('#img2'), 'Casper.visible() can detect if an element is visible');
         test.assert(!this.visible('#img3'), 'Casper.visible() can detect if an element is invisible');
+        test.assert(this.visible('#img4'), 'Casper.visible() can detect if an element with display:flex is visible');
         test.assert(this.visible('img'), 'Casper.visible() can detect if an element is visible');
         this.waitWhileVisible('#img1', function() {
             test.pass('Casper.waitWhileVisible() can wait while an element is visible');
