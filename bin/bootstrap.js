d98ff51df3e74b5a3900e2e13adc4544a7ffecc3
bughit <bughit@users.noreply.github.com>
2016-12-11 15:27:19 -0500

    don't create unnecessary globals in bootstrap.js

--- bin/bootstrap.js
+++ bin/bootstrap.js
@@ -42,39 +42,42 @@ if (!('phantom' in this)) {
 }
 
 // Common polyfills
+void function() {
 
-// cujos bind shim instead of MDN shim, see #1396
-var isFunction = function(o) {
-  return 'function' === typeof o;
-};
-var bind;
-var slice = [].slice;
-var proto = Function.prototype;
-var featureMap = {
-  'function-bind': 'bind'
-};
-function has(feature) {
-  var prop = featureMap[feature];
-  return isFunction(proto[prop]);
-}
-// check for missing features
-if (!has('function-bind')) {
-  // adapted from Mozilla Developer Network example at
-  // https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind
-  bind = function bind(obj) {
-    var args = slice.call(arguments, 1),
-      self = this,
-      nop = function() {
-      },
-      bound = function() {
-        return self.apply(this instanceof nop ? this : (obj || {}), args.concat(slice.call(arguments)));
+    // cujos bind shim instead of MDN shim, see #1396
+    var isFunction = function(o) {
+      return 'function' === typeof o;
+    };
+    var bind;
+    var slice = [].slice;
+    var proto = Function.prototype;
+    var featureMap = {
+      'function-bind': 'bind'
+    };
+    function has(feature) {
+      var prop = featureMap[feature];
+      return isFunction(proto[prop]);
+    }
+    // check for missing features
+    if (!has('function-bind')) {
+      // adapted from Mozilla Developer Network example at
+      // https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind
+      bind = function bind(obj) {
+        var args = slice.call(arguments, 1),
+          self = this,
+          nop = function() {
+          },
+          bound = function() {
+            return self.apply(this instanceof nop ? this : (obj || {}), args.concat(slice.call(arguments)));
+          };
+        nop.prototype = this.prototype || {}; // Firefox cries sometimes if prototype is undefined
+        bound.prototype = new nop();
+        return bound;
       };
-    nop.prototype = this.prototype || {}; // Firefox cries sometimes if prototype is undefined
-    bound.prototype = new nop();
-    return bound;
-  };
-  proto.bind = bind;
-}
+      proto.bind = bind;
+    }
+
+}();
 
 // Custom base error
 var CasperError = function CasperError(msg) {
