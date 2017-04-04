a80554b516456f365b569198f82853be2a86cffd
hlr1983 <henriquelagesrodrigues@gmail.com>
2017-04-04 03:55:11 -0300

    waitFor() fix + new tests

--- modules/casper.js
+++ modules/casper.js
@@ -2378,9 +2378,12 @@ Casper.prototype.waitFor = function waitFor(testFx, then, onTimeout, timeout, de
         this.waitStart();
         var start = new Date().getTime();
         var condition = false;
+        // set conditionTestedAfterTimeout to 1 to test condition not run one time after timeout when !condition
+        var conditionTestedAfterTimeout = 0;
         var interval = setInterval(function _check(self) {
             /*eslint max-statements: [1, 20]*/
-            if ((new Date().getTime() - start < timeout) && !condition) {
+            // refs #1805 #1806
+            if ( (!condition && (new Date().getTime() - start < timeout)) || (!condition && !conditionTestedAfterTimeout++) ) {
                 condition = testFx.call(self, self);
                 return;
             }
@@ -2412,7 +2415,6 @@ Casper.prototype.waitFor = function waitFor(testFx, then, onTimeout, timeout, de
         this.waiters.push(interval);
     });
 };
-
 /**
  * Waits until any alert is triggered.
  *
